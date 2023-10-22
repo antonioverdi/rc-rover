2ad168a791709c6db5f44434c305076bc22512e8
av <antonioverdicchio@gmail.com>
2023-10-22 13:31:13 -0700

    testing hardware

--- cat_tracker.py
+++ cat_tracker.py
@@ -144,12 +144,12 @@ def find_cat(net):
     for i in np.arange(0, predictions.shape[2]):
         confidence = predictions[0, 0, i, 2]
         if confidence > args["confidence"]:
-            if int(predictions[0, 0, i, 1]) == 7:
-                box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
-                detection_box = box.astype("int")
-                center = get_centerpoint[detection_box]
-                size = get_box_size(detection_box)
-                print(center, size)
+            #if int(predictions[0, 0, i, 1]) == 7:
+            box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
+            detection_box = box.astype("int")
+            center = get_centerpoint[detection_box]
+            size = get_box_size(detection_box)
+            print(center, size)
     return center, size
 
 
